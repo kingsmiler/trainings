74fe68ab84e17f81239f6bac3bc6f2096549a755
kingsmiler <kingsmiler@163.com>
2016-08-04 11:54:38 +0800

    add sample for reading post data in a servlet

---
+++ orm/orm-jdbc-web/src/main/java/org/xman/jdbc/servlet/HttpClientPostServlet.java
@@ -0,0 +1,43 @@
+package org.xman.jdbc.servlet;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+public class HttpClientPostServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(HttpClientPostServlet.class);
+
+    private static final String CHAR_SET = "UTF-8";
+
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding(CHAR_SET);
+        response.setCharacterEncoding(CHAR_SET);
+
+        String content = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+        System.out.println();
+        System.out.println(content);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(content);
+        writer.flush();
+    }
+}
