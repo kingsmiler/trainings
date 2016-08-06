2a4d7ad204e98045a03e7bda8f27ae94be3cad57
kingsmiler <kingsmiler@163.com>
2016-08-06 15:53:07 +0800

    add spring boot sample

---
+++ spring/spring-cloud/spring-cloud-getstart/src/main/java/org/xman/trainings/cloud/SpringCloudGetstartApplication.java
@@ -0,0 +1,12 @@
+package org.xman.trainings.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGetstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGetstartApplication.class, args);
+	}
+}
---
+++ spring/spring-cloud/spring-cloud-getstart/src/test/java/org/xman/trainings/cloud/SpringCloudGetstartApplicationTests.java
@@ -0,0 +1,16 @@
+package org.xman.trainings.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudGetstartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
